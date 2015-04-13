d99b3f831f8f96880ca2a663d5a8fe461fd9cccf
Orangen <heike.frauke@web.de>
2015-04-13 11:36:38 +0200

    icon herz wieder eingefügt

--- ButtonListeners.py
+++ ButtonListeners.py
@@ -20,8 +20,9 @@ class ButtonListenerSenderThread(threading.Thread):
         #if icon.get("icon",None) is "grinsSmilie":
             #GPIO.output(16, GPIO.HIGH)
         # Herz LED an
+        if icon == "herz"
         #if icon.get("icon",None) is "herz":
-            #GPIO.output(18, GPIO.HIGH)
+            GPIO.output(18, GPIO.HIGH)
         # Stern LED an
         #if icon.get("icon",None) is "stern":
             #GPIO.output(19, GPIO.HIGH)
